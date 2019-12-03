4d0172a56ecad8f493b50e52354e21551ff6c98f
gale34 <gales34@gmail.com>
2019-12-03 21:13:01 +0900

    feat: add racingCar test case

--- src/test/java/camp/nextstep/edu/racingcar/RacingCarTests.java
+++ src/test/java/camp/nextstep/edu/racingcar/RacingCarTests.java
@@ -3,6 +3,7 @@ package camp.nextstep.edu.racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
@@ -15,7 +16,7 @@ public class RacingCarTests {
 
     @DisplayName("자동차 이름 (5글자 이내) 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"blue", "red", "green"})
+    @ValueSource(strings = { "blue", "red", "green" })
     void carNormalNameTest(String input) {
         assertThat(new Car(input));
     }
@@ -35,27 +36,35 @@ public class RacingCarTests {
             new Car(null);
         }).isInstanceOf(IllegalArgumentException.class);
     }
-    
+
+    @DisplayName("자동차 이름 (NULL) @NullSource 테스트")
+    @ParameterizedTest
+    @NullSource
+    void carAbnormalNameTest(String input) {
+        assertThatThrownBy(() -> {
+            new Car(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     @DisplayName("자동차 움직임 테스트")
-    //@Disabled("Not implemented yet")
+    // @Disabled("Not implemented yet")
     void carMovingTest() {
         MovingStrategy mockMovingStrategy = mock(MovingStrategy.class);
         when(mockMovingStrategy.movable()).thenReturn(true);
-    
+
         Car car = new Car("test", 0);
         car.move(mockMovingStrategy);
         assertTrue(car.isInPosition(1));
     }
-    
 
     @Test
     @DisplayName("자동차 정지 테스트")
-    //@Disabled("Not implemented yet")
+    // @Disabled("Not implemented yet")
     void carNotMovingTest() {
         MovingStrategy mockMovingStrategy = mock(MovingStrategy.class);
         when(mockMovingStrategy.movable()).thenReturn(false);
-    
+
         Car car = new Car("test", 0);
         car.move(mockMovingStrategy);
         assertFalse(car.isInPosition(1));
