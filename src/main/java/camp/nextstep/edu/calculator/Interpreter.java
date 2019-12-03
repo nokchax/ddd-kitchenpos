dcd366ebf78831c6a7a8d47e827c51ae7317d626
nokchax <nokchax@gmail.com>
2019-12-03 20:01:54 +0900

    refactor : method extraction & remove useless field

--- src/main/java/camp/nextstep/edu/calculator/Interpreter.java
+++ src/main/java/camp/nextstep/edu/calculator/Interpreter.java
@@ -6,19 +6,13 @@ import java.util.regex.Pattern;
 public class Interpreter {
     private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
     private static final String DEFAULT_SPLITTER = "([,:])";
+
     private Numbers numbers;
-    private String splitter = DEFAULT_SPLITTER;
 
     static Interpreter of(String input) {
         Interpreter interpreter = new Interpreter();
 
-        Matcher matcher = PATTERN.matcher(input);
-        if(matcher.find()) {
-            interpreter.splitter = matcher.group(1);
-            input = matcher.group(2);
-        }
-
-        interpreter.numbers = new Numbers(input.split(interpreter.splitter));
+        interpreter.numbers = new Numbers(interpret(input));
 
         return interpreter;
     }
@@ -26,5 +20,17 @@ public class Interpreter {
     int calculate() throws RuntimeException {
         return this.numbers
                 .sum();
+    }
+
+    private static String[] interpret(String input) {
+        Matcher matcher = PATTERN.matcher(input);
+        String splitter = DEFAULT_SPLITTER;
+
+        if(matcher.find()) {
+            splitter = matcher.group(1);
+            input = matcher.group(2);
+        }
+
+        return input.split(splitter);
     }
 }
