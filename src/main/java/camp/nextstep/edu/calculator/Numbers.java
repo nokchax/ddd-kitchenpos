7ee0e23595a059700588cbb8bc4a715aec0a1eb1
nokchax <nokchax@gmail.com>
2019-12-03 19:36:07 +0900

    feat : [tdd] make numbers first class object

---
+++ src/main/java/camp/nextstep/edu/calculator/Numbers.java
@@ -0,0 +1,32 @@
+package camp.nextstep.edu.calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Numbers {
+    private List<Integer> numbers;
+
+    //boxed() -> primitive type을 reference type으로 박싱해서 stream을 생성함.
+    Numbers(String[] numbers) {
+        this.numbers = Arrays.stream(numbers)
+                .mapToInt(this::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+    
+    private int parseInt(String number) throws RuntimeException {
+        int parsedInt = Integer.parseInt(number);
+        
+        if(parsedInt < 0)
+            throw new RuntimeException();
+        
+        return parsedInt;
+    }
+
+    int sum() {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
