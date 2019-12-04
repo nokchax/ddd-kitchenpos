7abb91573775420b3fbb9293cae1a04f52c278c6
최준우 <junwoochoi@zuminternet.com>
2019-12-04 11:26:12 +0900

    refactor : 테스트를 위한 RandomMovingStrategy 클래스 변경
    RandomMovingStrategy에서 Random객체를 Mock하기 위한 변경

--- src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
+++ src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
@@ -3,9 +3,14 @@ package camp.nextstep.edu.racingcar;
 import java.util.Random;
 
 public class RandomMovingStrategy implements MovingStrategy {
+    private Random random;
+    public RandomMovingStrategy(Random random) {
+        this.random = random;
+    }
 
     @Override
     public boolean movable() {
-        return new Random().nextInt(9) >= 4;
+        random.setSeed(System.currentTimeMillis());
+        return random.nextInt(9) >= 4;
     }
 }
