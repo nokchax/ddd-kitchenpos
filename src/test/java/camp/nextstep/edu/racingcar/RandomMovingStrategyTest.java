00d03134acc37335ab39d129ca08fbe8ca25150b
최준우 <junwoochoi@zuminternet.com>
2019-12-05 08:21:16 +0900

    refactor : 경계값만 테스트하도록 변경

--- src/test/java/camp/nextstep/edu/racingcar/RandomMovingStrategyTest.java
+++ src/test/java/camp/nextstep/edu/racingcar/RandomMovingStrategyTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Random;
