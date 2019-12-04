448645e95148e499fe951d4b9524f268a39e7c7f
최준우 <junwoochoi@zuminternet.com>
2019-12-04 14:11:49 +0900

    style : 줄 간격 조정

--- src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
+++ src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class RandomMovingStrategy implements MovingStrategy {
     private Random random;
+
     public RandomMovingStrategy(Random random) {
         this.random = random;
     }
