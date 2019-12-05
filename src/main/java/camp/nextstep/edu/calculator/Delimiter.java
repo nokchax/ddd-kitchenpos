795f8d2a8ff84d49fd246f3ba8fe0cbb998cb62b
PARK SUNG DONG <oraedoa@gmail.com>
2019-12-05 16:31:30 +0900

    [refactor] 구분자 취급 객체 인터페이스 구현 및 코드 리팩토링.

---
+++ src/main/java/camp/nextstep/edu/calculator/Delimiter.java
@@ -0,0 +1,9 @@
+package camp.nextstep.edu.calculator;
+
+import java.util.List;
+
+public interface Delimiter {
+
+    List<String> splitStringReturnList(String line);
+
+}
---
+++ src/main/java/camp/nextstep/edu/calculator/DelimiterImpl.java
@@ -0,0 +1,49 @@
+package camp.nextstep.edu.calculator;
+
+import org.apache.logging.log4j.util.Strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
+
+public class DelimiterImpl implements Delimiter {
+
+    private static final Pattern EXTRACT_SEPARATOR_PATTERN = compile("//(.)\\n(.*)");
+    private static final String SEPARATOR_REGEX = "[:,]";
+
+    @Override
+    public List<String> splitStringReturnList(String line) {
+
+        List<String> tokenList = new ArrayList<>();
+
+        for(String token : useCustomSplitReturnArray(line)){
+            tokenList.addAll(
+                    Arrays.asList(useBasicSplitReturnArray(token))
+            );
+        }
+
+        return tokenList;
+    }
+
+    private String[] useCustomSplitReturnArray(String line) {
+
+        Matcher matcher = EXTRACT_SEPARATOR_PATTERN.matcher(line);
+
+        if(!matcher.find()) {
+            return useBasicSplitReturnArray(line);
+        }
+
+        String customDelimiter = matcher.group(1);
+
+        return matcher.group(2)
+                .split(customDelimiter);
+    }
+
+    private String[] useBasicSplitReturnArray(String line) {
+        return line.split(SEPARATOR_REGEX);
+    }
+}
