198a30e1d4b1fd71d224e173e87d991274fb4941
PARK SUNG DONG <oraedoa@gmail.com>
2019-12-05 16:32:02 +0900

    [refactor] 유효여부 취급 객체 인터페이스 구현 및 코드 리팩토링.

---
+++ src/main/java/camp/nextstep/edu/calculator/Validator.java
@@ -0,0 +1,7 @@
+package camp.nextstep.edu.calculator;
+
+public interface Validator {
+
+    int positiveOrElseThrow(String line);
+
+}
---
+++ src/main/java/camp/nextstep/edu/calculator/ValidatorImpl.java
@@ -0,0 +1,36 @@
+package camp.nextstep.edu.calculator;
+
+public class ValidatorImpl implements Validator{
+
+    @Override
+    public int positiveOrElseThrow(String line) {
+
+        if(line.isEmpty()) {
+            return 0;
+        }
+
+        int number = 0;
+
+        try {
+
+            number = Integer.parseInt(line);
+
+        } catch (Exception e) {
+
+            // 숫자 이외의 값.
+            this.executeRuntimeException(line);
+        }
+
+        if(number < 0) {
+
+            // 음수
+            this.executeRuntimeException(line);
+        }
+
+        return number;
+    }
+
+    private void executeRuntimeException(String line){
+        throw new RuntimeException(String.format("\"%d\" 값이 들어왔기 때문에 덧셈을 수행하지 못합니다.", line));
+    }
+}
