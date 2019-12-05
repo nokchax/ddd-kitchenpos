600afcf770707b1efb369f5433c395f8158814bc
PARK SUNG DONG <oraedoa@gmail.com>
2019-12-05 16:33:55 +0900

    [refactor] 중복 로직 제거, 클래스 멤버 제거, 생성자 final 할당 등.

--- src/main/java/camp/nextstep/edu/calculator/Calculator.java
+++ src/main/java/camp/nextstep/edu/calculator/Calculator.java
@@ -1,9 +1,6 @@
 package camp.nextstep.edu.calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static java.util.regex.Pattern.compile;
+import java.util.List;
 
 /**
  * Created by pasudo123 on 2019-12-04
@@ -11,75 +8,22 @@ import static java.util.regex.Pattern.compile;
  **/
 public class Calculator {
 
-    private final Pattern NUMBER_PATTERN = compile("([0-9]*)");
-    private final Pattern SEPARATOR_PATTERN = compile("([:,])");
-    private final Pattern EXTRACT_SEPARATOR_PATTERN = compile("//(.)\\n(.*)");
+    private final Validator validator;
+    private final Delimiter delimiter;
 
-    private void validateNumber(String paramNumber) {
-
-        if(!NUMBER_PATTERN.matcher(paramNumber).find() || Integer.parseInt(paramNumber) < 0){
-            throw new RuntimeException(String.format("\"%d\" 값이 들어왔기 때문에 덧셈을 수행하지 못합니다.", paramNumber));
-        }
+    public Calculator() {
+        this.validator = new ValidatorImpl();
+        this.delimiter = new DelimiterImpl();
     }
 
-    private boolean isZero(String line) {
-
-        return (line.isEmpty()
-                || !NUMBER_PATTERN.matcher(line).find());
-    }
-
-    private String[] getCustomPatternArray(String line){
-
-        Matcher matcher = EXTRACT_SEPARATOR_PATTERN.matcher(line);
-
-        if(!matcher.find()) {
-            return null;
-        }
-
-        String customDelimiter = matcher.group(1);
-
-        return matcher.group(2).split(customDelimiter);
-    }
-
-    int basicAdd(String line) {
-
-        if(isZero(line)) {
-            return 0;
-        }
+    int add(String line) {
 
         int sum = 0;
 
-        for(int i = 0; i < line.length(); i++) {
+        List<String>tokens = delimiter.splitStringReturnList(line);
 
-            char c = line.charAt(i);
-
-            // String.valueOf() vs literal
-            if(SEPARATOR_PATTERN.matcher( "" + c).find()){
-                continue;
-            }
-
-            validateNumber(c + "");
-            sum += Integer.parseInt("" + c);
-        }
-
-        return sum;
-    }
-
-    int customAdd(String line) {
-
-        if(isZero(line)) {
-            return 0;
-        }
-
-        int sum = 0;
-        String[]tokens = getCustomPatternArray(line);
-
-        if(tokens == null) {
-            return sum;
-        }
-
-        for (String token : tokens) {
-            sum += basicAdd(token);
+        for(String token : tokens) {
+            sum += validator.positiveOrElseThrow(token);
         }
 
         return sum;
