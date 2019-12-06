3ad9dc2859d5349bb6d9f0395cbff0ee92c2ec86
wonoh <wldnjsdh44121@nate.com>
2019-12-06 20:40:41 +0900

    refactor : 의미없는 1,2 숫자 변수화 및 한곳에서만 return 하게 수정

--- src/main/java/camp/nextstep/edu/calculator/Delimiter.java
+++ src/main/java/camp/nextstep/edu/calculator/Delimiter.java
@@ -8,17 +8,20 @@ public final class Delimiter {
     private Delimiter(){}
 
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
-    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final int DELIMITER_GROUP_INDEX = 1;
+    private static final int INPUT_GROUP_INDEX = 2;
 
-    public static String[] separate(final String text){
+    public static String[] separate(String text){
 
         final Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        String delimiter = DEFAULT_DELIMITER;
 
         if(matcher.find()){
-            String customDelimiter = matcher.group(1);
-            return matcher.group(2).split(customDelimiter);
+            delimiter = matcher.group(DELIMITER_GROUP_INDEX);
+            text = matcher.group(INPUT_GROUP_INDEX);
         }
-        return text.split(DEFAULT_DELIMITER);
+        return text.split(delimiter);
 
     }
 
