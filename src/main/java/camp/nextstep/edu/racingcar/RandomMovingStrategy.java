d52c5819dad680752df4cc2ad3196f3611aef762
PARK SUNG DONG <oraedoa@gmail.com>
2019-12-08 22:53:37 +0900

    [refactor] Mock Random 겍체 삽입을 위함.

--- src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
+++ src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
@@ -3,7 +3,14 @@ package camp.nextstep.edu.racingcar;
 import java.util.Random;
 
 public class RandomMovingStrategy implements MovingStrategy{
+
+    private Random random;
+
+    public RandomMovingStrategy(final Random random){
+        this.random = random;
+    }
+
     public boolean movable() {
-        return new Random().nextInt(9) >= 4;
+        return random.nextInt(9) >= 4;
     }
 }
